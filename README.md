96828b42ed970209c7ea56b4fecd10fdffa91430
Anthony <anthonyfdez23@gmail.com>
2022-06-16 23:42:37 -0400

    Added basing readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # php-blog
+
+php blog is a rest api for a simple blog where people can log in and read other people's blog posts and create their own
+
+## How to run locally
+
+To run this project locally you need to download XAMPP to be able to run the local MySQL database and run the code in a local server
+
+After you have XAMPP installed import the database from dbTemplate.sql in the root of the project.
+
+## Api Docs
+
+There are protected api routes that the user needs to be logged in to access, for those you always need to pass a JSON Web Token generated after creating the user or logging in, and also the email of the user. This are passed in the header of the POST request
+
+| Header        | Value                    |
+| ------------- | ------------------------ |
+| Authorization | Pass the JWT here        |
+| Email         | Pass the user email here |
+
+| Api                | Endpoint                          | Params                            | Send data as    |
+| ------------------ | --------------------------------- | --------------------------------- | --------------- |
+| Create new User    | "/blog/api/users/signup.php"      | "email", "password"               | JSON            |
+| Login user         | "/blog/api/users/login.php"       | "email", "password"               | JSON            |
+| Check if Logged In | "/blog/api/users/is_logged_in.php | Auth Header                       | Headers         |
+| Sign out user      | "/blog/api/users/signout.php      | Auth Header                       | Headers         |
+| Sign out all       | "/blog/api/users/signout_all.php  | Auth Header                       | Headers         |
+| Delete             | "/blog/api/users/delete           | "email", "password", Auth Headers | JSON && Headers |
---
+++ dbTemplate.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jun 17, 2022 at 05:28 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authTokens`
+--
+
+CREATE TABLE `authTokens` (
+  `email` text DEFAULT NULL,
+  `createdAt` date NOT NULL DEFAULT current_timestamp(),
+  `token` text DEFAULT NULL,
+  `userId` int(11) NOT NULL,
+  `tokenId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `created_at`) VALUES
+(1, 'Technology', '2022-06-11 19:07:31'),
+(2, 'Gaming', '2022-06-11 19:07:31'),
+(3, 'Auto', '2022-06-11 19:07:31'),
+(4, 'Entertainment', '2022-06-11 19:07:31'),
+(5, 'Books', '2022-06-11 19:07:31');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `category_id`, `title`, `body`, `author`, `created_at`) VALUES
+(1, 1, 'Technology Post One', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut interdum est nec lorem mattis interdum. Cras augue est, interdum eu consectetur et, faucibus vel turpis. Etiam pulvinar, enim quis elementum iaculis, tortor sapien eleifend eros, vitae rutrum augue quam sed leo. Vivamus fringilla, diam sit amet vestibulum vulputate, urna risus hendrerit arcu, vitae fringilla odio justo vulputate neque. Nulla a massa sed est vehicula rhoncus sit amet quis libero. Integer euismod est quis turpis hendrerit, in feugiat mauris laoreet. Vivamus nec laoreet neque. Cras condimentum aliquam nunc nec maximus. Cras facilisis eros quis leo euismod pharetra sed cursus orci.', 'Sam Smith', '2022-06-11 19:07:31'),
+(2, 2, 'Gaming Post One', 'Adipiscing elit. Ut interdum est nec lorem mattis interdum. Cras augue est, interdum eu consectetur et, faucibus vel turpis. Etiam pulvinar, enim quis elementum iaculis, tortor sapien eleifend eros, vitae rutrum augue quam sed leo. Vivamus fringilla, diam sit amet vestibulum vulputate, urna risus hendrerit arcu, vitae fringilla odio justo vulputate neque. Nulla a massa sed est vehicula rhoncus sit amet quis libero. Integer euismod est quis turpis hendrerit, in feugiat mauris laoreet. Vivamus nec laoreet neque.', 'Kevin Williams', '2022-06-11 19:07:31'),
+(3, 1, 'Technology Post Two', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut interdum est nec lorem mattis interdum. Cras augue est, interdum eu consectetur et, faucibus vel turpis. Etiam pulvinar, enim quis elementum iaculis, tortor sapien eleifend eros, vitae rutrum augue quam sed leo. Vivamus fringilla, diam sit amet vestibulum vulputate, urna risus hendrerit arcu, vitae fringilla odio justo vulputate neque. Nulla a massa sed est vehicula rhoncus sit amet quis libero. Integer euismod est quis turpis hendrerit, in feugiat mauris laoreet. Vivamus nec laoreet neque. Cras condimentum aliquam nunc nec maximus. Cras facilisis eros quis leo euismod pharetra sed cursus orci.', 'Sam Smith', '2022-06-11 19:07:31'),
+(4, 4, 'Entertainment Post One', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut interdum est nec lorem mattis interdum. Cras augue est, interdum eu consectetur et, faucibus vel turpis. Etiam pulvinar, enim quis elementum iaculis, tortor sapien eleifend eros, vitae rutrum augue quam sed leo. Vivamus fringilla, diam sit amet vestibulum vulputate, urna risus hendrerit arcu, vitae fringilla odio justo vulputate neque. Nulla a massa sed est vehicula rhoncus sit amet quis libero. Integer euismod est quis turpis hendrerit, in feugiat mauris laoreet. Vivamus nec laoreet neque. Cras condimentum aliquam nunc nec maximus. Cras facilisis eros quis leo euismod pharetra sed cursus orci.', 'Mary Jackson', '2022-06-11 19:07:31'),
+(5, 4, 'Entertainment Post Two', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut interdum est nec lorem mattis interdum. Cras augue est, interdum eu consectetur et, faucibus vel turpis. Etiam pulvinar, enim quis elementum iaculis, tortor sapien eleifend eros, vitae rutrum augue quam sed leo. Vivamus fringilla, diam sit amet vestibulum vulputate, urna risus hendrerit arcu, vitae fringilla odio justo vulputate neque. Nulla a massa sed est vehicula rhoncus sit amet quis libero. Integer euismod est quis turpis hendrerit, in feugiat mauris laoreet. Vivamus nec laoreet neque. Cras condimentum aliquam nunc nec maximus. Cras facilisis eros quis leo euismod pharetra sed cursus orci.', 'Mary Jackson', '2022-06-11 19:07:31'),
+(6, 1, 'Technology Post Three', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut interdum est nec lorem mattis interdum. Cras augue est, interdum eu consectetur et, faucibus vel turpis. Etiam pulvinar, enim quis elementum iaculis, tortor sapien eleifend eros, vitae rutrum augue quam sed leo. Vivamus fringilla, diam sit amet vestibulum vulputate, urna risus hendrerit arcu, vitae fringilla odio justo vulputate neque. Nulla a massa sed est vehicula rhoncus sit amet quis libero. Integer euismod est quis turpis hendrerit, in feugiat mauris laoreet. Vivamus nec laoreet neque. Cras condimentum aliquam nunc nec maximus. Cras facilisis eros quis leo euismod pharetra sed cursus orci.', 'Sam Smith', '2022-06-11 19:07:31'),
+(7, 1, 'This is the title', 'This is the body', 'anthony', '2022-06-11 21:25:14'),
+(8, 1, 'This is the title', 'This is the body', 'anthony', '2022-06-11 21:29:51'),
+(9, 1, 'Test', 'Body', 'Anthony', '2022-06-11 21:44:32'),
+(10, 1, 'Test', 'Body', 'Anthony', '2022-06-11 21:48:39'),
+(11, 1, 'Test', 'Body', 'Anthony', '2022-06-11 21:50:08'),
+(12, 1, 'Test', 'Body', 'Anthony', '2022-06-11 21:52:39'),
+(13, 1, 'Test', 'Body', 'Anthony', '2022-06-11 21:53:38'),
+(14, 1, 'Test', 'Body', 'Anthony', '2022-06-12 01:09:12'),
+(15, 1, 'Test', 'Body', 'Anthony', '2022-06-12 01:10:02'),
+(16, 1, 'this is the awesome title', 'this si the amazing body', 'me myself and i', '2022-06-12 01:21:13'),
+(17, 1, 'this is the awesome title', 'this si the amazing body', 'me myself and i', '2022-06-12 01:22:32'),
+(18, 1, 'Yoo thisis sisiiii', 'this si the amazing body', 'me myself and i', '2022-06-12 01:22:52'),
+(19, 1, 'New post', 'This is the body of the post', 'me', '2022-06-13 10:39:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` mediumtext NOT NULL,
+  `password` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`) VALUES
+(66, 'anthony', '$2y$10$e/.nEtXPVEdOfk21JCU.cOmtssSUSs.IG52CQogKHmb78xsYlq3FO');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `authTokens`
+--
+ALTER TABLE `authTokens`
+  ADD PRIMARY KEY (`tokenId`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `email` (`email`) USING HASH;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `authTokens`
+--
+ALTER TABLE `authTokens`
+  MODIFY `tokenId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
